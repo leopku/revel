2f5532fdc3a5a48594d879f2412dad298bbf1db1
leopku <leopku@qq.com>
2017-07-11 23:15:04 +0800

    fixed font color of colored tag

--- src/components/TopicDetailJumbotron.vue
+++ src/components/TopicDetailJumbotron.vue
@@ -2,7 +2,7 @@
   <section id="topic-detail">
     <Jumbotron :xs="22" :sm="22" class="flex-justify--start">
       <section slot="jumbotron-content">
-        <el-tag type="gray" v-for="tag in topic.tags" key="tag.objectId" :color="tag.color">{{tag.title}}</el-tag>
+        <el-tag type="gray" v-for="tag in topic.tags" key="tag.objectId" :color="tag.color" :class="{ 'fg-white': tag.hasOwnProperty('color') }">{{tag.title}}</el-tag>
         <p class="text-emphasis--small font-weight--500">{{topic.title}}</p>
         <p class="text-regular--small">{{topic.content}} </p>
       </section>
@@ -21,7 +21,8 @@ export default {
   props: {
     topic: {
       type: Object,
-      require: true
+      require: true,
+      default: { tags: [] }
     }
   }
 }
