9e5a7b32ea153f74d08ac65974a23c3f5b61962e
leopku <leopku@qq.com>
2017-07-21 00:38:14 +0800

    up voting

--- cloud/main.js
+++ cloud/main.js
@@ -2,12 +2,38 @@
 * @Author: leopku
 * @Date:   2017-06-27 19:20:30
 * @Last Modified by:   leopku
-* @Last Modified time: 2017-07-20 15:30:13
+* @Last Modified time: 2017-07-21 00:37:35
 */
 'use strict'
 
+const { createVote } = require('./util')
+
 /* global Parse */
-Parse.Cloud.define('hello', (req, res) => res.success('Hi'))
+Parse.Cloud.define('hello', (req, res) => {
+  const query = new Parse.Query('Vote')
+  // query.equalTo('upVotedBy', new Parse.User({ objectId: 'fi5pHA9iEm' }))
+  query.get('r8ZQ8VXY0R')
+    .then(vote => {
+      const relation = vote.relation('upVotedBy')
+      // relation.equalTo('objectId', 'fi5pHA9iEm')
+      const relationQuery = relation.query()
+      relationQuery.equalTo('objectId', 'fi5pHA9iEm')
+      relationQuery.find({useMasterKey: true})
+        .then(results => {
+          console.log(results)
+          res.success('Hi')
+        })
+        .catch(error => res.error(error))
+    })
+    .catch(error => res.error(error))
+  // query.find()
+  //   .then(results => {
+  //     console.log(results)
+  //     res.success('Hi')
+  //   })
+  //   .catch(() => res.error('failed'))
+  // res.success('Hi')
+})
 
 Parse.Cloud.define('signup', (req, res) => {
   let user = new Parse.User()
@@ -20,23 +46,70 @@ Parse.Cloud.define('signup', (req, res) => {
 })
 
 Parse.Cloud.define('upVote', (req, res) => {
-  const user = req.user
-  console.log(user)
-  console.log(req.params.replyId)
-  console.log(req.params.userId)
-  return res.success('vote successfully')
+  const voteQuery = new Parse.Query('Vote')
+  const replyId = req.params.replyId
+  if (!replyId || !req.user) {
+    res.success('Vote successfully')
+    return
+  }
+  const reply = new Parse.Object('Reply', { objectId: replyId })
+  voteQuery.equalTo('reply', reply)
+  voteQuery.find()
+    .then(results => {
+      if (results.length === 0) {
+        // never voted before, create first
+        const vote = createVote({
+          reply,
+          user: req.user,
+          action: 'upVotedBy',
+          upVotedCount: 1
+        })
+
+        vote.save(null, {useMasterKey: true})
+          .then(result => res.success('vote successfully'))
+          .catch(error => res.error(error))
+      } else {
+        // already voted
+        const vote = results[0]
+        const relation = vote.relation('upVotedBy')
+        const relationQuery = relation.query()
+        relationQuery.equalTo('objectId', req.user.id)
+        relationQuery.find({useMasterKey: true})
+          .then(results => {
+            if (results.length === 0) {
+              relation.add(req.user)
+              vote.increment('upVotedCount')
+              console.log('*** add ***')
+            } else {
+              relation.remove(req.user)
+              let upVotedCount = vote.get('upVotedCount')
+              upVotedCount--
+              vote.set('upVotedCount', upVotedCount)
+              console.log('*** remove ***')
+            }
+
+            vote.save(null, {useMasterKey: true})
+              .then(result => res.success('voted successfully'))
+              .catch(error => res.error(error))
+          })
+          .catch(error => res.error(error))
+      }
+    })
+    .catch(error => res.error(error))
 })
 
 Parse.Cloud.beforeSave('Reply', (req, res) => {
   const user = req.user
-
-  if (req.object.get('author')) {
-    delete req.object.author
+  if (req.object.isNew()) {
+    if (req.object.get('author')) {
+      delete req.object.author
+    }
+    req.object.set('author', {
+      '__type': 'Pointer',
+      className: '_User',
+      objectId: user.id
+    })
   }
-  req.object.set('author', {
-    '__type': 'Pointer',
-    className: '_User',
-    objectId: user.id
-  })
+
   res.success(req.object)
 })
---
+++ cloud/util.js
@@ -0,0 +1,58 @@
+/*
+* @Author: leopku
+* @Date:   2017-07-20 22:01:12
+* @Last Modified by:   leopku
+* @Last Modified time: 2017-07-20 22:19:07
+*/
+
+'use strict'
+
+/* global Parse */
+
+/**
+ * [createVote description]
+ * @param  {Object} options.reply          [description]
+ * @param  {Object} options.user           [description]
+ * @param  {String} options.action         'upVotedBy' or 'downVotedBy'
+ * @param  {Number} options.upVotedCount   [description]
+ * @param  {Number} options.downVotedCount [description]
+ * @param  {Array}  options.roleNames      [description]
+ * @return {[type]}                        [description]
+ */
+function createVote ({
+  reply,
+  user,
+  action,
+  upVotedCount = 0,
+  downVotedCount = 0,
+  roleNames = []
+} = {}) {
+  const Vote = Parse.Object.extend('Vote')
+  const vote = new Vote()
+
+  // deal with relations
+  const relation = vote.relation(action)
+  relation.add(user)
+
+  // deal with reply
+  vote.set('reply', reply)
+
+  const acl = new Parse.ACL()
+  acl.setPublicReadAccess(true)
+  acl.setWriteAccess('level0', true)
+  acl.setWriteAccess('Moderators', true)
+  for (var i = roleNames.length - 1; i >= 0; i--) {
+    acl.setWriteAccess(roleNames[i], true)
+  }
+  vote.setACL(acl)
+
+  // deal with vote count
+  vote.set('upVotedCount', upVotedCount)
+  vote.set('downVotedCount', downVotedCount)
+
+  return vote
+}
+
+module.exports = {
+  createVote: createVote
+}
